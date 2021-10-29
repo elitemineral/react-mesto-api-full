a3ab3779831ad758b373e06254351b3011925e81
Дмитрий Богданов <bogdanov@it-limon.ru>
2021-10-29 14:32:00 +0500

    Убрал sameSite

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -19,7 +19,6 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: 'none',
       }).send({ message: 'Авторизация прошла успешно' });
     })
     .catch(next);
